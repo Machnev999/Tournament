5df1c3f92c0f19686b9e0b72440c403f447231f5
Kirill Machnev <machnev999@gmail.com>
2022-11-08 19:32:13 +0300

    add Game,Player>NotReg,GameTest

---
+++ src/main/java/ru/netology/domain/Game.java
@@ -0,0 +1,54 @@
+package ru.netology.domain;
+
+
+import java.util.ArrayList;
+
+public class Game {
+    public Game() {
+        this.players = players;
+    }
+    protected ArrayList<Player> players = new ArrayList<>();
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void register(Player player) { // регистрация пользователя
+        players.add(player);
+    }
+
+    public int round(String PlayerName1, String PlayerName2) {
+        Player p1 = null;
+        Player p2 = null;
+        for (Player player : players) {
+            if (player.getName().equals(PlayerName1)) { // имя которое берем равно имени игрока
+            p1 = player;
+            }
+            if (player.getName().equals(PlayerName2)) {
+                p2 = player;
+
+            }
+        }
+
+// если имя не зарег ( null )  - выдаем ошибку
+        if (p1 == null) {
+            throw new NotRegisteredException( PlayerName1 + " is not registered");
+        }
+        if (p2 == null) {
+            throw new NotRegisteredException(PlayerName2 + " is not registered");
+        }
+
+// правило игры. сравниваем силу
+        if (p1.getStrength() == p2.getStrength()) {
+            return 0; // ничья
+        }
+        if (p1.getStrength() > p2.getStrength()) {
+            return 1; // выйграл 1 игрок
+        } else {
+            return 2; // выйграл 2 игрок
+
+        }
+    }
+
+
+}
---
+++ src/test/java/GameTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Game;
+import ru.netology.domain.NotRegisteredException;
+import ru.netology.domain.Player;
+
+
+
+public class GameTest {
+
+
+    Game game = new Game();
+
+    Player player1 = new Player(1, "Batman", 200);
+    Player player2 = new Player(2, "Superman", 400);
+    Player player3 = new Player(3, "Ironman", 200);
+
+
+    @BeforeEach
+    @Test
+    public void setUp() {
+        game.register(player1);
+        game.register(player2);
+        game.register(player3);
+
+    }
+
+    @Test
+    public void shouldDraw() {
+        Assertions.assertEquals(0, game.round("Batman", "Ironman"));
+    }
+
+    @Test
+    public void shouldWinPlayerName1() {
+        Assertions.assertEquals(1, game.round("Superman", "Ironman"));
+    }
+
+    @Test
+    public void shouldWinPlayerName2() {
+        Assertions.assertEquals(2, game.round("Batman", "Superman"));
+    }
+
+    @Test
+    public void shouldNoTRegisteredPlayerName1() {
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            game.round("Halk", "IronMan");
+        });
+    }
+
+    @Test
+    public void shouldNoTRegisteredPlayerName2() {
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            game.round("IronMan", "Halk");
+        });
+    }
+    @Test
+    public void shouldNoTRegisteredNoOne() {
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            game.round("Grut", "Halk");
+        });
+    }
+
+}
