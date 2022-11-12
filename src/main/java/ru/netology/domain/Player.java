4f2a1fdca14d49d1086aec435c25cb8951e821ca
Kir <87722197+Machnev999@users.noreply.github.com>
2022-11-12 19:52:08 +0300

    Update Player.java

--- src/main/java/ru/netology/domain/Player.java
+++ src/main/java/ru/netology/domain/Player.java
@@ -14,17 +14,7 @@ public class Player {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Integer getStrength() {
         return strength;
     }
-
-    public void setStrength(Integer strength) {
-        this.strength = strength;
-    }
-
-
 }
